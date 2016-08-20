5b83bbb2446c04216de9fee2beab95a758822b40
nadim <i.nadim@gmail.com>
2016-08-20 01:21:51 -0400

    Improved people page

---
+++ client/src/components/ProfileHead.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+export default class ProfileHead extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {pos:0, toggle:false};
+    this.mousemove = this.mousemove.bind(this);
+  }
+
+  mousemove(e) {
+      let body = document.body.getBoundingClientRect();
+      let elem = this.elem.getBoundingClientRect();
+      let mouse = [e.clientX, e.clientY];
+      let center = [body.left+elem.left+80, body.top+80+elem.top];
+      
+
+      let rad = Math.atan2(mouse[0] - center[0], mouse[1] - center[1]);
+      let pos = Math.round((rad + Math.PI)/(0.25*Math.PI)+1);
+      pos = (pos == 9) ? 1 : pos
+      this.setState({pos:pos});
+  }
+  componentDidMount() {
+    document.body.addEventListener('mousemove', this.mousemove, false);
+
+  }
+  componentWillUnmount() {
+    document.body.removeEventListener('mousemove', this.mousemove, false);
+  }
+
+  render() {
+    let {people, image} = this.props;
+    let style = (image) ? {
+                  backgroundImage:'url(/people.jpg)',
+                  backgroundPositionY: `-${167*this.props.pos}px`,
+                  backgroundPositionX: `-${167*this.state.pos}px`} : {}
+    
+    return (
+              <div>
+                <div className='profile-head' ref={(ref) => this.elem = ref} style={style}
+                  onClick={()=>this.setState({toggle:!this.state.toggle})}
+                >
+
+                    <div className="element" 
+                      style={{
+                            position: 'absolute',
+                            bottom: '0',
+                            background: '#1a3445',
+                            color: 'white',
+                            padding: '2px 6px 5px 6px',
+                            fontWeight: 'bold'}}>{people.name.toLowerCase().split(' ')[0][0]+people.name.toLowerCase().split(' ')[1][0]}
+
+
+                    </div>
+                       <div className="description" style={this.props.last ? {right: '0'}: {}}>
+                        <div className="name">{people.name}</div>
+                       
+                        <div className="title">{people.title}</div>
+                        <div className="knowledge">{people.knowledge}</div>
+
+                      </div>
+                </div>
+
+
+              </div>
+    );
+  }
+}
--- client/src/containers/People.js
+++ client/src/containers/People.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
+import ProfileHead from '../components/ProfileHead';
 export default class People extends Component {
   constructor(props) {
     super(props);
@@ -11,43 +11,46 @@ export default class People extends Component {
            title: 'Co-Founder + Managing Partner',
            knowledge: 'Blockchain, Medical Technology, Artificial Intelligence, etc.'
           },
-          {name: 'Alexandra Lutchman',
-           title: 'Co-Founder + Partner',
-           knowledge: 'Education, Non-Profits, Civic Works, etc.'
-          },
+
           {name: 'Imran Jameel',
            title: 'Co-Founder + Partner',
            knowledge: ' IoT, Robotics, Consumer Electronics, etc.'
           },
-          {name: 'Nadim Islam',
-          title: 'Co-Founder + Partner',
-          knowledge: 'Full Stack Developer, Data Visualization, UI/UX'},
+          {name: 'Alexandra Lutchman',
+           title: 'Co-Founder + Partner',
+           knowledge: 'Education, Non-Profits, Civic Works, etc.'
+          },
+          
           {name: 'Alex Daskalov',
           title: 'Co-Founder + Partner',
           knowledge: ' Blockchain, Medical Technology, Wearables, etc.'},
+          {name: 'Nadim Islam',
+          title: 'Co-Founder + Partner',
+          knowledge: 'Full Stack Developer, Data Visualization, UI/UX, etc.'},
 
           {name: 'Miti Bhavsar',
-          title: 'Associate'},
+          title: 'Associate', knowledge:'education, finance, block chain, etc.'},
           {name: 'Ayvi Islam', knowledge: 'Product Design, Mechanical Engineering, etc.'},
           {name: 'Colin Gallacher', knowledge : 'Robotics, Augmented Reality, Haptics, etc. '},
           {name: 'Zafarali Ahmed', knowledge : 'Biosimulation, Cancer Genomics, Machine Learning, etc.'},
           {name: 'Anjali Chandrashekar', knowledge : 'Product Design'},
           {name: 'Mohammad Rafsan', knowledge : 'Distributed Computing'},
     ];
+    this.state = {pos: 1}
   }
   render() {
     return (
       <div className="flexbox-container mainpage">
+        
         <div className="container">
-        <ul className="people">
-          {this.peoples.map(people => 
-            <li>
-              <div className="name">{people.name}</div>
+        <h1 style={{marginBottom:'2em'}}>people</h1>
+        <div className="people">
+          {this.peoples.map((people,i) => 
+            <div className="person">
+              <ProfileHead pos={i} people={people} last={(i+1)%5 == 0 } image={(i < 5)} />
 
-              <div className="knowledge">{people.knowledge}</div>
-              <div className="title">{people.title}</div>
-            </li>)}
-        </ul>
+            </div>)}
+        </div>
         </div>
       </div>
     );
