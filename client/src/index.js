17e27b8284bd460514c2eb0754f288ec9dc763e2
Imran Jameel <imranjameel@Imrans-MacBook-Pro-2.local>
2016-08-23 20:02:27 -0400

    remove unused dependency

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'velocity-animate';
 
 import { Provider } from 'react-redux'
 import { Router, Route, browserHistory } from 'react-router'
