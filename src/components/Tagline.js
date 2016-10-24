27fb65d067118a121114130999d26460eb4859c4
nadim <i.nadim@gmail.com>
2016-10-24 08:45:01 -0400

    Removes THREE renderer and changes animation front page

--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -6,7 +6,11 @@ export default class Tagline extends Component {
   constructor(props) {
     super(props);
     let self = this;
-    this.state = {done:false};
+    this.state = {done:false, set2: [
+      'ambient intelligence',
+      'decentralized systems',
+      'blockchain technology'
+    ]};
 
     this.set1 = [
       'technologists',
@@ -14,15 +18,7 @@ export default class Tagline extends Component {
       'engineers'
     ];
 
-    this.set2 = [
-      '',
-      'ambient intelligence',
-      'blockchain',
-      'decentralized systems',
-    ];
-
     this.set3 = [
-      '',
       'healthcare',
       'finance',
       'education'
@@ -32,36 +28,37 @@ export default class Tagline extends Component {
   componentDidMount() {
     let self = this;
     setTimeout(()=> {
-    let textHeight = self.box1.clientHeight / self.set1.length;
-    console.log(textHeight);
-    self.tlp1.to(self.main, 0.5, {css:{opacity:1}, delay:0})
-      .to(self.part1, 0.6, {css:{opacity:1}, delay:0});
+      
+      self.tlp1.to(self.main, 0.5, {css:{opacity:1}, delay:0})
+        .to(self.part1, 0.6, {css:{opacity:1}, delay:0});
 
-    for(let i = 1; i < self.set1.length; i++) {
-      let y = -((i * textHeight) % (self.set1.length*textHeight));
-      self.tlp1.to(self.box1, 0.2, {css:{y:y}, delay:0.6});
-    }
-    self.tlp1.to(self.part2, 0.5, {css:{opacity:1}, delay:0});
+      for(let i = 1; i < self.set1.length; i++) {
+        self.tlp1.to(self.box1, 0.6, {text:this.set1[i], delay:0.7});
+      }
+      self.tlp1.to(self.part21, 0.6, {css:{opacity:1}, delay:0.7});
+      self.tlp1.to(self.part22, 0.6, {css:{opacity:1}, delay:0.7});
 
-    for(let i = 1; i < self.set2.length; i++) {
-      let y = -((i * textHeight) % (self.set2.length*textHeight));
-      self.tlp1.to(self.box2, 0.2, {css:{y:y}, delay:0.6});
-    }
-    self.tlp1.to(self.part3, 0.6, {css:{opacity:1}, delay:0});
+      self.tlp1.to(self.part3, 0.6, {css:{opacity:1}, delay:0.7});
 
-    for(let i = 1; i < self.set3.length; i++) {
-      let y = -((i * textHeight) % (self.set3.length*textHeight));
-      self.tlp1.to(self.box3, 0.2, {css:{y:y}, delay:0.6});
-    }
+      self.tlp1.to(self.box3, 0.8, {text:'healthcare', delay:0});
+      self.tlp1.to(self.box3, 0.8, {text:'healthcare, finance', delay:0});
+      self.tlp1.to(self.box3, 1.2, {text:'healthcare, finance & education', delay:0});
 
-    self.tlp1
-    .to(self.part3, 0.5, {css:{opacity:0}, delay:2})
-    .to(self.part2, 0.5, {css:{opacity:0}, delay:0})
-    .to(self.part1, 0.5, {css:{opacity:'0'}, delay:0})
-    .to(self.finallogo, 1.0, {text:'rhodium.', delay:1, onComplete:()=> {
-      self.tlp1.clear();
-    }})
-    }, 0)
+      self.tlp1
+      .to(self.finallogo, 0.5,{css:{opacity:1}, delay:0})
+      .to(self.finallogo, 1.0, {text:'we are rhodium.', delay:0, onComplete:()=> {
+        self.tlp1.clear();
+      }})
+    }, 0);
+
+    setInterval(() => {
+      console.log('interval');
+      let shiftedSet = this.state.set2.slice(1,this.state.set2.length).concat(this.state.set2[0]);
+      console.log(shiftedSet);
+      this.setState({
+        set2: shiftedSet
+      });
+    }, 4000);
 
 
   }
@@ -75,35 +72,23 @@ export default class Tagline extends Component {
      <div className="logo flexbox-container" >
 
         <div ref={(ref) => this.main = ref} style={{textAlign:'center', opacity:'0'}}>
-          <span> we are </span> <span ref={(ref) => this.finallogo = ref} ></span>
           
           <div ref={(ref) => this.part1 = ref} style={{opacity:'0'}}>
-            <div className='alternator'>
-              <ul ref={(ref) => this.box1 = ref}>
-                {this.set1.map(word => <li><p>{word}</p></li>)}
-              </ul>
-            </div>
+            <span> we are </span> <span className='alternator' ref={(ref) => this.box1 = ref}>{this.set1[0]}</span>
           </div>
-          <div ref={(ref) => this.part2 = ref} style={{opacity:'0'}}>
-          <span > who specialize in </span>
-
-            <div className='alternator' style={{textAlign:'left'}}>
-              <ul  ref={(ref) => this.box2 = ref}>
-                {this.set2.map(word => <li className={'animations glitch'}><p>{word}</p></li>)}
-              </ul>
-
-            </div>
+          <div ref={(ref) => this.part21 = ref} style={{opacity:'0'}}>
+            <span> who specialize in </span>
+          </div>
+          <div ref={(ref) => this.part22 = ref} style={{opacity:'0'}}>
+            <span className='alternator glitch' style={{minWidth:'300px', textAlign:'left'}} ref={(ref) => this.box2 = ref} data-text={this.state.set2[0]}>{this.state.set2[0]}</span>
           </div>
           <div ref={(ref) => this.part3 = ref} style={{opacity:'0'}}>
-          <span> for </span>
-          <div className='alternator' style={{textAlign:'left'}}>
-            <ul ref={(ref) => this.box3 = ref}>
-              {this.set3.map(word => <li><p>{word}</p></li>)}
-            </ul>
+            <span> for </span>
+            <span className='alternator' ref={(ref) => this.box3 = ref}></span>
           </div>
-          </div>
+          <br/>
+          <div style={{opacity:'0'}} ref={(ref) => this.finallogo = ref}>we are</div>
         </div>
-
       </div>
     );
   }
--- src/components/TrianglifyContainer.js
+++ src/components/TrianglifyContainer.js
@@ -1,46 +1,26 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import THREE from "three-canvas-renderer";
 
 export default class TrianglifyComponent extends Component {
   constructor(props) {
     super(props);
-
-    // construct the position vector here, because if we use 'new' within render,
-    // React will think that things have changed when they have not.
-    this.cameraPosition = new THREE.Vector3(0, 0, 0);
     this.animate = this.animate.bind(this);
-    this.state = {
-      cubeRotation: new THREE.Vector3(),
-    };
-
-    this.SEPARATION = 10, this.AMOUNTX = 60, this.AMOUNTY = 40;
-    this.mouseX = 0; this.mouseY = 0;
-    this.camera = null;
-    this.scene = null;
-    this.renderer = null;
-    this.particles = new Array();
-    
+    this.SEPARATION = 50, this.AMOUNTX = 40, this.AMOUNTY = 40;
+    this.SEPARATION = 20;
   }
 
   componentDidMount() {
     this.count = 0.05;
     this.animate();
   }
-
-
   componentWillUnmount() {
     window.cancelAnimationFrame(this.animationFrame);
   }
-  render() {
-
-  }
   animate() {
-      console.log("animate!");
       this.container.width = window.innerWidth;
       this.container.height = window.innerHeight;
-      this.SEPARATION = 50;
+      
       this.context = this.container.getContext('2d');
       this.context.clearRect(0,0,window.innerWidth,window.innerHeight);
 
@@ -49,16 +29,21 @@ export default class TrianglifyComponent extends Component {
       
       for( var iz = 1; iz < 30; iz ++) {
          for ( var ix = 0; ix < this.AMOUNTX; ix ++ ) {
-            var x =  ix * (this.SEPARATION - iz*0.5);
-            var y = ( Math.sin( ( ix + this.count+ iz) * 0.3) + 1 )*3 ;
-            var scale = ( Math.sin( ( iz + this.count ) * 0.3 ) + 1 )+
-              ( Math.sin( ( ix + this.count ) * 0.5 ) + 1 );
+            var xSpacing = (this.SEPARATION + iz*3);
+            var ySpacing = (this.SEPARATION + iz)/4;
+            var xOffset = window.innerWidth - this.AMOUNTX*xSpacing;
+            var amplitude = 2.5*(30-iz) ;
+
+            var x =  xOffset/2.4 + xSpacing*ix;
+            var verticalWave = Math.sin( ( ix/2 + iz + this.count/2) * 0.3 );
+            var horizontalWave = Math.sin( ( ix/2 +this.count/4) ) *0.3;
+            var y = (iz)*ySpacing+ (amplitude* verticalWave) + 5*iz*horizontalWave  ;
+
+            var radius = (2-verticalWave)*(iz)/20;
+            
             this.context.beginPath();
-            this.context.arc(x, 500+ y*8 - (40 - iz)*(iz), scale*10/iz ,0,Math.PI*2,false);
-            this.context.fillStyle = "black";
-            if(iz == 1) this.context.fillStyle = "red";
-            if(iz == 17) this.context.fillStyle = "blue";
-            this.context.stroke();
+            this.context.arc(x, y + (window.innerHeight*0.7), radius,0,Math.PI*2,false);
+            this.context.fillStyle = "#193446";
             this.context.fill();
          }
       }
@@ -66,41 +51,6 @@ export default class TrianglifyComponent extends Component {
       requestAnimationFrame( this.animate )
   }
 
-  onWindowResize() {
-
-
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
-
-  }
-
-  renderThree() {
-
-        this.count = 0.05;
-        let i = 0;
-
-        for ( let ix = 0; ix < this.AMOUNTX; ix ++ ) {
-
-          for ( let iy = 0; iy < this.AMOUNTY; iy ++ ) {
-
-            let particle = this.particles[ i++ ];
-            particle.position.y = ( Math.sin( ( ix + this.count ) * 0.3 ) * 50 ) +
-              ( Math.sin( ( iy + this.count ) * 0.5 ) * 50 );
-            particle.scale.x = particle.scale.y = ( Math.sin( ( ix + this.count ) * 0.3 ) + 1 ) * 4 +
-              ( Math.sin( ( iy + this.count ) * 0.5 ) + 1 ) * 4;
-
-          }
-
-        }
- 
-
-        this.renderer.render( this.scene, this.camera );
-
-        this.count += 0.05;
-
-  }
 
   render() {
     const width = window.innerWidth; // canvas width
