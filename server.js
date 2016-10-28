6cf3a1c8191e2533b78936a2ee75a505101a794e
nadim <i.nadim@gmail.com>
2016-10-27 23:33:39 -0400

    updates glitch animations

--- server.js
+++ server.js
@@ -42,8 +42,12 @@ const renderFullPage = (html, initialState) => {
         <meta name="msapplication-square310x310logo" content="/favico/mstile-310x310.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="rhodium is a technology studio with expertise in ambient intelligence and decentralized systems. We assemble multidisciplinary teams to tackle challenges in healthcare, education, and financial services.">
+          
+        <!-- for Facebook -->          
+        <meta property="og:image" content="/favico/mstile-310x310.png" />
+
+
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
         ${(process.env.NODE_ENV === 'production') ? '<link rel="stylesheet" href="/dist/styles.css">' : '' }
         <script>
--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -6,10 +6,14 @@ export default class Tagline extends Component {
   constructor(props) {
     super(props);
     let self = this;
-    this.state = {done:false, set2: [
-      'ambient intelligence',
-      'decentralized systems',
-      'blockchain technology'
+    this.startGlitching = this.startGlitching.bind(this);
+    this.state = {
+      done:false, 
+      ghost: 'ambient intelligence',
+      set2: [
+        'ambient intelligence',
+        'decentralized systems',
+        'blockchain technology'
     ]};
 
     this.set1 = [
@@ -33,7 +37,9 @@ export default class Tagline extends Component {
         .to(self.part1, 0.6, {css:{opacity:1}, delay:0});
 
       for(let i = 1; i < self.set1.length; i++) {
-        self.tlp1.to(self.box1, 0.6, {text:this.set1[i], delay:0.7});
+        self.tlp1.to(self.box1, 0.5, {text: '', delay:0.7});
+        self.tlp1.to(self.box1, 0.5, {text: this.set1[i], delay:0});
+        
       }
       self.tlp1.to(self.part21, 0.6, {css:{opacity:1}, delay:0.7});
       self.tlp1.to(self.part22, 0.6, {css:{opacity:1}, delay:0.7});
@@ -47,24 +53,29 @@ export default class Tagline extends Component {
       self.tlp1
       .to(self.finallogo, 0.5,{css:{opacity:1}, delay:0})
       .to(self.finallogo, 1.0, {text:'we are rhodium.', delay:0, onComplete:()=> {
-        self.tlp1.clear();
+        this.startGlitching();
       }})
     }, 0);
-
-    setInterval(() => {
-      console.log('interval');
-      let shiftedSet = this.state.set2.slice(1,this.state.set2.length).concat(this.state.set2[0]);
-      console.log(shiftedSet);
-      this.setState({
-        set2: shiftedSet
-      });
-    }, 4000);
 
 
   }
 
+  startGlitching() {
+    this.interval = setInterval(() => {
+    this.setState({ghost: this.state.set2[1]});
+
+      setTimeout(() => {
+        let shiftedSet = this.state.set2.slice(1,this.state.set2.length).concat(this.state.set2[0]);
+        this.setState({
+          set2: shiftedSet,
+          ghost: shiftedSet[0]
+        });
+      }, 700);
+    }, 4000);
+  }
   componentWillUnmount() {
     this.tlp1.clear();
+    clearInterval(this.interval);
   }
 
   render() {
@@ -79,8 +90,11 @@ export default class Tagline extends Component {
           <div ref={(ref) => this.part21 = ref} style={{opacity:'0'}}>
             <span> who specialize in </span>
           </div>
-          <div ref={(ref) => this.part22 = ref} style={{opacity:'0'}}>
-            <span className='alternator glitch' style={{minWidth:'300px', textAlign:'left'}} ref={(ref) => this.box2 = ref} data-text={this.state.set2[0]}>{this.state.set2[0]}</span>
+          <div ref={(ref) => this.part22 = ref} style={{opacity:'0', position: 'relative', textAlign:'center'}}>
+            <div className='alternator glitch-before'>{this.state.ghost}</div>
+             <div className='alternator glitch-after'>{this.state.ghost}</div>
+            <div className='alternator glitch' style={{textAlign:'center'}} ref={(ref) => this.box2 = ref}>{this.state.set2[0]}</div>
+           
           </div>
           <div ref={(ref) => this.part3 = ref} style={{opacity:'0'}}>
             <span> for </span>
